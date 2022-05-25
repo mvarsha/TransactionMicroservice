6dd77435398969d1594238565b6f6afd8cd3e9b7
Varsha Holla <varsha.holla>
2022-05-26 09:26:56 +1000

    Security

---
+++ src/main/java/com/accenture/techtask/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.accenture.techtask.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().antMatchers("/h2-console/**");
+    }
+}
